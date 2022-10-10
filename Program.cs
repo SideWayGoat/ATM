0eb21891214b043028e86c74e548ad8438370473
Patrik Skattberg <patrikskattberg@hotmail.com>
2022-10-10 14:08:54 +0200

    RemovedJaggedArray Fixed IsBanking Method

--- Program.cs
+++ Program.cs
@@ -6,10 +6,11 @@ namespace ATM
     {
         static void Main(string[] args)
         {
+            int AccountNumber;
             int LogInAttempts = 0;
-            bool IsBanking = false;
-            string[,] UserAndPassword = new string[5, 2] { { "Patrik", "1234" }, { "Anas", "2345" },{ "Theo", "3456" },{ "Leo", "4567" },{ "Lucas", "5678" } };
-            double[][] Cash = new double[5][];
+            bool Banking = false;
+            string[,] UserAndPassword = new string[5, 2] { { "Patrik", "1234" }, { "Anas", "2345" }, { "Theo", "3456" }, { "Leo", "4567" }, { "Lucas", "5678" } };
+            double[,] Money = new double[5, 2] { { 2345.23 , 45562 }, { 2312, 23214 }, { 5313.4, 43144 }, { 4341.23, 45366 }, { 2344.12, 55324 } };
             do
             {
                 Console.Clear();
@@ -21,39 +22,25 @@ namespace ATM
                 {
                     if(UserAndPassword[i,0] == UserName && UserAndPassword[i,1] == UserPassword)
                     {
-                        IsBanking = true;
+                        Banking = true;
+                        AccountNumber = i;
                         LogInAttempts = 0;
+                        IsBanking(Banking,Money,UserAndPassword,AccountNumber);
                     }
                 }
-                //if (UserName == UserAndPassword[0, 0] && UserPassword == UserAndPassword[0, 1])
-                //{
-                //    Console.WriteLine("Välkommen {0} ", UserAndPassword[0, 0]);
-                //    Banking = true;
-                //}                
-                //else if (UserName == UserAndPassword[1, 0] && UserPassword == UserAndPassword[1, 1])
-                //{
-                //    Console.WriteLine("Välkommen {0} ", UserAndPassword[1, 1]);
-                //    Banking = true;
-                //}                
-                //else if (UserName == UserAndPassword[2, 0] && UserPassword == UserAndPassword[2, 1])
-                //{
-                //    Console.WriteLine("Välkommen {0} ", UserAndPassword[2, 0]);
-                //    Banking = true;
-                //}                
-                //else if (UserName == UserAndPassword[3, 0] && UserPassword == UserAndPassword[3, 1])
-                //{
-                //    Console.WriteLine("Välkommen {0} ", UserAndPassword[3, 0]);
-                //    Banking = true;
-                //}                
-                //else if (UserName == UserAndPassword[4, 0] && UserPassword == UserAndPassword[4, 1])
-                //{
-                //    Console.WriteLine("Välkommen {0} ", UserAndPassword[4, 0]);
-                //    Banking = true;
-                //}
                 LogInAttempts++;
-            } while (LogInAttempts < 3 ^ IsBanking == true);
+            } while (LogInAttempts < 3 ^ Banking == true);
 
-            while (IsBanking)
+        }
+
+        private static void PrintMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("1. Se dina konton och saldo\n2. Överföring mellan konton\n3. Ta ut pengar\n4. Logga ut");
+        }
+        public static void IsBanking(bool Banking, double[,] Cash, string[,] UserAndPassword, int AccountNumber)
+        {
+            while (Banking)
             {
                 PrintMenu();
                 if (Int32.TryParse(Console.ReadLine(), out int MenuChoice))
@@ -62,16 +49,24 @@ namespace ATM
                     {
                         case 1:
                             // Se konton och saldo
+                            Console.WriteLine("Översikt av konton:");
+                            ShowAccountDetails(Cash, AccountNumber);
+                            Console.ReadKey();
                             break;
                         case 2:
                             //Överföring mellan konton
+                            Console.WriteLine("Överföring mellan konto:");
+                            ShowAccountDetails(Cash, AccountNumber);
+                            Console.ReadKey();
                             break;
                         case 3:
                             // Ta ut pengar 
+                            ShowAccountDetails(Cash, AccountNumber);
+                            Console.ReadKey();
                             break;
                         case 4:
                             // Logga ut
-                            IsBanking = false;
+                            Banking = false;
                             break;
                         default:
                             Console.WriteLine("Menu val är giltigt mellan 1-4");
@@ -85,13 +80,12 @@ namespace ATM
                     Console.ReadKey();
                 }
             }
-
         }
 
-        private static void PrintMenu()
+        private static void ShowAccountDetails(double[,] Cash, int AccountNumber)
         {
-            Console.Clear();
-            Console.WriteLine("1. Se dina konton och saldo\n2. Överföring mellan konton\n3. Ta ut pengar\n4. Logga ut");
+            Console.WriteLine("Privatkonto:  {0} kr", Cash[AccountNumber, 0]);
+            Console.WriteLine("Lönekonto:    {0} kr", Cash[AccountNumber, 1]);
         }
     }
 }
